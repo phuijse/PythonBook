81e9efff9c1a349786d711cdda4e9d73c491532e
Pablo Huijse <pablo.huijse@gmail.com>
2019-07-21 12:56:10 -0400

    avance clases profiling, optimización y paralelismo

--- unidad3/fractal.py
+++ unidad3/fractal.py
@@ -1,28 +1,22 @@
-def make_fractal(N, maxiter=50):
+def evaluate_z(zi, zr, maxiters=50, cr=-0.835, ci=-0.2321):
+    nit = 0
+    zi2 = zi**2
+    zr2 = zr**2
+    while zi2 + zr2 <= 4. and nit < maxiters:
+        zi = 2*zr*zi + ci
+        zr = zr2 - zi2 + cr
+        zr2 = zr**2
+        zi2 = zi**2 
+        nit +=1
+    return nit
+    
+def make_fractal(N, maxiters=50):
     image = []
-    cr, ci = -0.835, -0.2321
-    # cr, ci = -0.1, 0.65
-    def initialize(i, j):
-        zi = -1.0 + i*2/N
-        zr = -2.0 + j*2/N
-        return zi, zr
-    
-    def evaluate(zi, zr):
-        nit = 0
-        zi2 = zi*zi
-        zr2 = zr*zr
-        while zi2 + zr2 <= 4. and nit < maxiter:
-            zi = 2*zr*zi + ci
-            zr = zr2 - zi2 + cr
-            zr2 = zr*zr
-            zi2 = zi*zi 
-            nit +=1
-        return nit
-    
     for i in range(N):
         row = []
         for j in range(2*N):
-            zi, zr = initialize(i, j)
-            row.append(evaluate(zi, zr)/maxiter)
+            zi = -1.0 + i*2/N
+            zr = -2.0 + j*2/N
+            row.append(evaluate_z(zi, zr, maxiters))
         image.append(row)
     return image
