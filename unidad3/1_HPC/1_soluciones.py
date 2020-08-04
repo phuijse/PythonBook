19824bd1d8ca996fe0e3b25361d1aef0015154ea
Pablo Huijse <pablo.huijse@gmail.com>
2020-08-03 22:39:45 -0400

    typos y videos

--- unidad3/1_HPC/1_soluciones.py
+++ unidad3/1_HPC/1_soluciones.py
@@ -1,32 +1,36 @@
 
 # Midiendo tiempo con %timeit
-
+from fractal import make_fractal, evaluate_z
 
 # Parte 1
-for N in [5, 10, 50, 100, 500]:
-    fractal_image = make_fractal(N, maxiters=50)
-    fig, ax = plt.subplots(figsize=(8, 4), tight_layout=True)
-    ax.imshow(fractal_image, aspect='equal', cmap=plt.cm.viridis, origin='lower')
-    ax.axis('off');
+N = [5, 10, 50, 100, 500]
+fig, ax = plt.subplots(len(N), 1, figsize=(4, 8), tight_layout=True)
+for k, n in enumerate(N):
+    fractal_image = make_fractal(n, maxiters=50)    
+    ax[k].imshow(fractal_image, aspect='equal', cmap=plt.cm.viridis, origin='lower')
+    ax[k].axis('off');
 
 # Parte 2
-%timeit -r10 -n5 make_fractal(N=500, maxiters=50)
+%timeit -r10 make_fractal(N=500, maxiters=50)
 
 # Parte 3
-times = []
-N = [5, 10, 50, 100, 500]
-for n in N:
-    result = %timeit -r10 -n5 -o -q make_fractal(N=n, maxiters=50)
-    times.append(result)
+results = []
+for k, n in enumerate(N):
+    result = %timeit -r10 -q -o make_fractal(n, maxiters=50)
+    results.append(result)
     
-fig, ax = plt.subplots()
-ax.errorbar(N, 
-            [time.average for time in times],  
-            [time.stdev for time in times])
+for n, result in zip(N, results):
+    print(n, result.best, result.average, result.worst)
+
+# Parte 4
+fig, ax = plt.subplots(figsize=(5, 3), tight_layout=True)
+ax.errorbar(N,
+            [result.average for result in results],
+            [result.stdev for result in results])
 ax.set_xlabel('N')
 ax.set_ylabel('Tiempo [s]')
-ax.set_yscale('log')
 ax.set_xscale('log')
+ax.set_yscale('log')
 
 
 # Midiendo tiempo con lprun
