a24034f031fcd6cffa7429fe7636061c2659ed89
Pablo Huijse <pablo.huijse@gmail.com>
2021-04-01 23:00:36 -0300

    Actividad jupyter y numpy

---
+++ actividades/unidad1/2_solucion.py
@@ -0,0 +1,10 @@
+# Fractal de Mandelbrot
+
+x = np.linspace(-2, 1, num=2000)
+y = np.linspace(-1.5, 1.5, num=2000)
+X, Y = np.meshgrid(x, y)
+C = X + 1j*Y
+Z = np.zeros_like(C)
+
+for i in range(1000):
+    Z = Z*Z + C
---
+++ actividades/unidad1/script_interesante.py
@@ -0,0 +1,10 @@
+
+def saludar(nombre=""):
+    """
+    Una función absurda para saludar
+    """
+    print(f"Hola {nombre}")
+    print("Chao") 
+
+if __name__ == "__main__":
+    saludar("Pablo")
